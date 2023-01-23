55cea1175d22f0be22fc371efcceb583fd74877d
Rauan Ishida Sanfelice <rauan.sanfelice@gmail.com>
2023-01-23 19:15:38 -0300

    :white_check_mark: create tests api

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,16 +1,15 @@
-import logging
-import json
 import datetime
-
+import json
+import logging
 from decimal import Decimal
 from http import HTTPStatus
 
 from django.conf import settings
-from rest_framework.test import APIClient
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-from pipeline.models import Audiencia, TempoDisponivel
 from api.views import transform_json
+from pipeline.models import Audiencia, TempoDisponivel
 
 logger = logging.getLogger(__name__)
 urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [""])
@@ -39,12 +38,16 @@ class APIListAudienciaTestCase(TestCase):
         result_json = json.loads(response.content.decode("utf-8"))
         audiencia = Audiencia.objects.get(id=1)
         result_audiencia = result_json["results"][0]
-        date_exhibition_date = datetime.datetime.strptime(result_audiencia["exhibition_date"], "%Y-%m-%d").date()
+        date_exhibition_date = datetime.datetime.strptime(
+            result_audiencia["exhibition_date"], "%Y-%m-%d"
+        ).date()
         self.assertEqual(result_json["count"], 1)
         self.assertEqual(result_audiencia["signal"], audiencia.signal)
         self.assertEqual(result_audiencia["program_code"], audiencia.program_code)
         self.assertEqual(date_exhibition_date, audiencia.exhibition_date)
-        self.assertEqual(Decimal(result_audiencia["average_audience"]), audiencia.average_audience)
+        self.assertEqual(
+            Decimal(result_audiencia["average_audience"]), audiencia.average_audience
+        )
 
 
 class APIListTempoDisponivelTestCase(TestCase):
@@ -64,7 +67,9 @@ class APIListTempoDisponivelTestCase(TestCase):
         result_json = json.loads(response.content.decode("utf-8"))
         audiencia = TempoDisponivel.objects.get(id=1)
         result_audiencia = result_json["results"][0]
-        date_date = datetime.datetime.strptime(result_audiencia["date"], "%Y-%m-%d").date()
+        date_date = datetime.datetime.strptime(
+            result_audiencia["date"], "%Y-%m-%d"
+        ).date()
         self.assertEqual(result_json["count"], 1)
         self.assertEqual(result_audiencia["signal"], audiencia.signal)
         self.assertEqual(result_audiencia["program_code"], audiencia.program_code)
@@ -85,10 +90,18 @@ class TransformJsonToListTestCase(TestCase):
                 "program_code": "MOCK",
                 "weekday": 6,
                 "available_time": {
-                    "mean": 1, "median": 2, "sum": 3, "max": 4, "min": 5,
+                    "mean": 1,
+                    "median": 2,
+                    "sum": 3,
+                    "max": 4,
+                    "min": 5,
                 },
                 "predicted_audience": {
-                    "mean": 6, "median": 7, "sum": 8, "max": 9, "min": 10,
+                    "mean": 6,
+                    "median": 7,
+                    "sum": 8,
+                    "max": 9,
+                    "min": 10,
                 },
             }
         ]
@@ -112,14 +125,52 @@ class APIListAnaliticoTestCase(TestCase):
         response = self.client.get("/api/analitico/programa/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
         result_json = json.loads(response.content.decode("utf-8"))
-        mock_result = {'signal': 'SP1', 'program_code': 'PTV1', 'weekday': 4, 'available_time': {'mean': 933891.96, 'median': 933891.96, 'sum': 933891.96, 'max': 933891.96, 'min': 933891.96}, 'predicted_audience': {'mean': 10.0, 'median': 10.0, 'sum': 10, 'max': 10, 'min': 10}}
+        mock_result = {
+            "signal": "SP1",
+            "program_code": "PTV1",
+            "weekday": 4,
+            "available_time": {
+                "mean": 933891.96,
+                "median": 933891.96,
+                "sum": 933891.96,
+                "max": 933891.96,
+                "min": 933891.96,
+            },
+            "predicted_audience": {
+                "mean": 10.0,
+                "median": 10.0,
+                "sum": 10,
+                "max": 10,
+                "min": 10,
+            },
+        }
         self.assertEqual(result_json[0], mock_result)
 
     def test_api_list_analitico_por_periodo_retorna_sucesso(self):
         "Should get list of Analicio por periodo"
 
-        response = self.client.get("/api/analitico/periodo/?exhibition_date_inicio=05%2F06%2F2020&exhibition_date_fim=05%2F06%2F2020")
+        response = self.client.get(
+            "/api/analitico/periodo/?exhibition_date_inicio=05%2F06%2F2020&exhibition_date_fim=05%2F06%2F2020"
+        )
         self.assertEqual(response.status_code, HTTPStatus.OK)
         result_json = json.loads(response.content.decode("utf-8"))
-        mock_result = {'signal': 'SP1', 'program_code': 'PTV1', 'weekday': 4, 'available_time': {'mean': 933891.96, 'median': 933891.96, 'sum': 933891.96, 'max': 933891.96, 'min': 933891.96}, 'predicted_audience': {'mean': 10.0, 'median': 10.0, 'sum': 10, 'max': 10, 'min': 10}}
+        mock_result = {
+            "signal": "SP1",
+            "program_code": "PTV1",
+            "weekday": 4,
+            "available_time": {
+                "mean": 933891.96,
+                "median": 933891.96,
+                "sum": 933891.96,
+                "max": 933891.96,
+                "min": 933891.96,
+            },
+            "predicted_audience": {
+                "mean": 10.0,
+                "median": 10.0,
+                "sum": 10,
+                "max": 10,
+                "min": 10,
+            },
+        }
         self.assertEqual(result_json[0], mock_result)
