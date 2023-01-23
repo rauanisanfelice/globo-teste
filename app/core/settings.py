d1beb6766063cda469c221988b1f4453f0021985
Rauan Ishida Sanfelice <rauan.sanfelice@gmail.com>
2023-01-23 19:14:53 -0300

    :rotating_light: fix linter

--- app/core/settings.py
+++ app/core/settings.py
@@ -81,9 +81,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
-    ],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "MAX_PAGINATE_BY": 100,
